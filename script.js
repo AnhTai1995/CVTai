523430a56491fa5faa231af2c973cc0e1d3a8f12
AnhTai1995 <anhtai.hcmus95@gmail.com>
2016-05-06 21:38:44 +0700

    Last commit

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
  */
 
 
-var app = angular.module('mycv', ["xeditable"]);
+var app = angular.module('app', ["xeditable"]);
 app.run(function (editableOptions) {
     editableOptions.theme ="bs3"
 });
+
 app.controller('Ctrller', function($scope, $http) {
     $http.get("JSON/data.json")
         .then(function(response) {
@@ -20,4 +21,40 @@ app.controller('Ctrller', function($scope, $http) {
             $scope.myeducation = response.data.education;
         });
 
+});
+
+app.controller('CtrlSummary', function($scope){
+    $scope.addItem = function() {
+        $scope.mySummary.push({ 'id': $scope.mySummary.length+1, 'summaryvalue': 'new Summary', isNew: true });
+    };
+});
+
+app.controller('CtrlExperience', function($scope){
+    $scope.addItem = function() {
+        $scope.myExperience.push({ 'experience': 'new experience', isNew: true });
+    };
+});
+
+app.controller('CtrlProject', function($scope){
+    $scope.addItem = function() {
+        $scope.myproject.push({ 'project': 'new project', isNew: true });
+    };
+});
+
+app.controller('CtrlLanguage', function($scope){
+    $scope.addItem = function() {
+        $scope.myLanguage.push({ 'language': 'new language', isNew: true });
+    };
+});
+
+app.controller('CtrlSkill', function($scope){
+    $scope.addItem = function() {
+        $scope.myskill.push({ 'skill': 'new skill', isNew: true });
+    };
+});
+
+app.controller('CtrlEducation', function($scope){
+    $scope.addItem = function() {
+        $scope.myeducation.push({ 'education': 'new education', isNew: true });
+    };
 });
